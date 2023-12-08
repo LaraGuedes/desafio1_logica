644b7e972ca1cdc0cca9e5d8a9825f5387a9f7c3
Lara Calegario Guedes <102930080+LaraGuedes@users.noreply.github.com>
2023-12-08 18:02:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("Ano novo está chegando!!");
+
+let nome = "Mariah Carey";
+let idade = 54;
+let anoAtual = 2023;
+let dataNascimento = (anoAtual - idade);
+
+// console.log(nome);
+// console.log(idade);
+// console.log(anoAtual);
+console.log(dataNascimento);
+
+let quartaFeira = false;
+
+console.log(quartaFeira);
+
+let suaIdade = 15;
+if (suaIdade >= 18){
+    console.log(" Você é maior de idade, poderá curtir o show do Roberto Carlos com participação especial de Mariah Carey.");
+} else{
+    console.log("Sinto muito, mas assistirá da tv globo");
+}
+
+let mesDeAniversário = "JUNHO";
+if (mesDeAniversário == "JUNHO" || mesDeAniversário == "AGOSTO" || mesDeAniversário == "DEZEMBRO"){
+    console.log(`Uma ou mais opções estão corretas, O mês escolhido foi o mês ${mesDeAniversário}`);
+}else{
+    console.log(`Algo de errado não está certo, o mês digitado foi o mês ${mesDeAniversário}`)
+}
